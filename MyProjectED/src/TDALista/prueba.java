0bee894ebe07238011a965fc82b34fe8d08199d7
joaqo <joaqo@DESKTOP-2MOPJKG.fibertel.com.ar>
2021-09-27 19:51:54 -0300

    prueba escritorio

--- MyProjectED/src/TDALista/prueba.java
+++ MyProjectED/src/TDALista/prueba.java
@@ -3,5 +3,8 @@ package TDALista;
 public class prueba {
 	public static void main(String [] args) {
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
 	}
 }
