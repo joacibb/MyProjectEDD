2c2ddc40cf0a533d00d4f1e925dc2b304f84c771
joaqo <joaqo@DESKTOP-2MOPJKG.fibertel.com.ar>
2021-09-27 19:00:11 -0300

    Dnodo y ListaIntercalada(incompleta)

---
+++ MyProjectED/src/Ejercicios/ListaIntercalada.java
@@ -0,0 +1,16 @@
+package Ejercicios;
+
+import TDALista.ListaSE;
+import TDALista.Nodo;
+import TDALista.PositionList;
+
+public class ListaIntercalada{
+	
+	public static PositionList<Integer> intercalar(PositionList<Integer> l1, PositionList<Integer> l2){
+		PositionList<Integer> nueva = new ListaSE<Integer>();
+		boolean corte;
+		int aux;
+		return l2;
+	}
+	
+}
---
+++ MyProjectED/src/TDALista/DNodo.java
@@ -0,0 +1,36 @@
+package TDALista;
+
+public class DNodo<E> implements Position<E>{
+private E element;
+private DNodo<E> previo,siguiente;
+
+public DNodo(DNodo<E>p, DNodo<E> s, E e) {
+	previo = p;
+	siguiente = s;
+	element = e;
+}
+
+public E element() {
+	return element;
+}
+
+public DNodo<E> getNext(){
+	return siguiente;
+}
+
+public DNodo<E> getPrev(){
+	return previo;
+}
+
+public void setNext(DNodo<E> s) {
+	siguiente = s;
+}
+
+public void setPrev(DNodo<E> p) {
+	previo = p;
+}
+
+public void setElement(E e) {
+	element=e;
+}
+}
